63ef1aa8bc18fe99f47e10c7dd714ca37b219e12
Jordan Worm <jordan.worm96@gmail.com>
2020-03-02 13:47:05 -0600

    Added boilerplate and starter test.

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    "jest/globals": true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'jest',
+  ],
+  rules: {
+    "no-console": "off",
+    "no-unused-vars": "off",
+    "no-plusplus": "off",
+    "spaced-comment": "off",
+    "no-restricted-syntax": "off",
+    "guard-for-in" : "off",
+    "consistent-return": "off",
+    "jsx-a11y/alt-text": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "jsx-a11y/no-noninteractive-element-interactions": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "no-access-state-in-setstate": "off",
+    "max-len": "off",
+      //React Rules
+    "react/jsx-one-expression-per-line": "off",
+    "react/forbid-prop-types": "off",
+    "react/prop-types": "off",
+    "react/no-unused-prop-types": "off",
+    "react/no-deprecated": "off",
+    "react/jsx-filename-extension": "off",
+    "react/destructuring-assignment": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/no-access-state-in-setstate": "off",
+    "import/extensions": "off",
+    "no-array-index-key": "off",
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, './App');
+const DIST_DIR = path.join(__dirname, './Dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        options: {
+          'presets': ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
+    ]
+  }
+};
