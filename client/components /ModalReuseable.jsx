efeed9aad167c91fd0edd23af874239d01b4c91a
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-03-02 15:34:59 -0600

    Start remaking modal skeleton

--- client/components /ModalReuseable.jsx
+++ client/components /ModalReuseable.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { Modal } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Modal, Button} from 'react-bootstrap';
 
 export default function ModalReuseable() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
+      <Button variant="primary" onClick={handleShow}>
+        Open Modal button- example
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Render Title component here</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modal-body">Here is where the component will go...</Modal.Body>
+      </Modal>
+
       <span>This is a modal</span>
     </div>
   );
