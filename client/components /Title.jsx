47400820dc04b2464cf7bd00da1c37f1302fb90e
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-03-02 15:12:55 -0600

    Reconfigure to make eslint happy

--- client/components /Title.jsx
+++ client/components /Title.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-  export default function Title(){
-    
-    return (
-      <div>
-        <nav class="navbar navbar-light" style={{backgroundColor:'orange', color: 'white'}}>
-          <span class ='nav-brand mb-0 h1'>Wevent</span>
-          <button class ="btn btn-sm btn-outline-secondary" type="button">button</button>
-        </nav>
-      </div>
-    )
-  }
+export default function Title() {
+  return (
+    <div>
+      <nav className="navbar navbar-light" style={{ backgroundColor: 'orange', color: 'white' }}>
+        <span className="nav-brand mb-0 h1">Wevent</span>
+        <button className="btn btn-sm btn-outline-secondary" type="button">button</button>
+      </nav>
+    </div>
+  );
+}
