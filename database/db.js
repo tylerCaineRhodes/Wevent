ebdbff92ccb1dc706a8a5c852d6c2550b3cb5a42
Benjamin Pernick <bpernick@comcast.net>
2020-03-02 21:45:10 -0600

    write queries and routes to serve dashboard and event info components

--- database/db.js
+++ database/db.js
@@ -3,6 +3,105 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
    host: 'localhost',
    user: 'root',
-   password: 'password',
-   database: 'bradley',
+   password: 'Phs@203386',
+   database: 'Wevent',
 });
+
+
+db.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+ 
+  console.log('Connected to the MySQL server.');
+});
+
+//routes for dashboard
+module.exports.getHostingEventsForDashboard = (host_id, cb) => {
+
+   let query = `SELECT e.title, u_e.pending FROM events AS e LEFT JOIN users_events_attending AS u_e ON e.event_id = u_e.event_id WHERE e.host_id = ?`
+   db.query(query, [host_id], (err, data)=>{
+      if (err){
+         cb(err);
+         return;
+      }
+      cb(null, data);
+   })
+}
+
+module.exports.getAttendingEventsForDashboard = (user_id, cb) => {
+   let query = `SELECT e.title, u_e.pending FROM events AS e INNER JOIN users_events_attending AS u_e ON e.event_id = u_e.event_id WHERE u_e.user_id = ?`
+   db.query(query, [user_id], (err, data)=>{
+      if (err){
+         cb(err);
+         return;
+      }
+      cb(null, data);
+   })
+}
+
+module.exports.getNameAndLocation = (user_id, cb) => {
+   let query = `SELECT display_name, location_state, location_city FROM users WHERE user_id = ?`
+   db.query(query, [user_id], (err, data)=>{
+      if (err){
+         cb(err);
+         return;
+      }
+      cb(null, data);
+   })
+}
+
+// routes for event info
+
+module.exports.getEventInfoForConditionalRender = (event_id, user_id, cb) => {
+   //mysql UNION expects the same # of columns in both queries. The query for 'user_id' serves only to add a second column to the second query
+   let query = `SELECT host_id, private FROM events WHERE event_id = ? UNION SELECT pending, user_id from users_events_attending WHERE event_id = ? AND user_id = ?`
+   db.query(query, [event_id, event_id, user_id], (err, data)=>{
+      if (err){
+         cb(err);
+         return;
+      }
+      cb(null, data);
+   })
+}
+
+module.exports.getEventInfoForHost = (event_id, cb) => {
+   let query = `select e.title, e.description, e.date, e.time, e.price, e.address_1, e.address_2, e.zipcode, e.city, e.state, e.attendance_max, e.attendance_current, u_e.pending, u.display_name from events AS e LEFT JOIN users_events_attending AS u_e ON e.event_id = u_e.event_id INNER JOIN users AS u ON u_e.user_id = u.user_id WHERE e.event_id = ?`
+   db.query(query, [event_id], (err, data)=>{
+      if (err){
+         cb(err);
+         return;
+      }
+      cb(null, data);
+   })
+}
+
+module.exports.getEventInfoForNonHost = (event_id, hasAccess, cb) => {
+   if (hasAccess){
+      let query = `select title, description, date, time, price, address_1, address_2, zipcode, city, state, attendance_max, attendance_current FROM events WHERE event_id = ?`
+      db.query(query, [event_id], (err, data)=>{
+         if (err){
+            cb(err);
+            return;
+         }
+         cb(null, data);
+      })
+   } else {
+      let query = `select title, description, date, time, price, city, state, attendance_max, attendance_current FROM events WHERE event_id = ?`
+      db.query(query, [event_id], (err, data)=>{
+         if (err){
+            cb(err);
+            return;
+         }
+         cb(null, data);
+      })
+   }
+}
+
+module.exports.getCalendarEvents = (filters, cb) =>{
+
+}
+
+module.exports.logIn = (filters, cb) =>{
+
+}
--- server/server.js
+++ server/server.js
@@ -16,7 +16,84 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 
 //connections/queries
+app.get('/dashboard', (req, res) =>{
+  db.getAttendingEventsForDashboard(req.body.user_id, (err, attending) => {
+    if(err){
+      console.error(err);
+      res.sendStatus(500);
+      return;
+    }
+    db.getHostingEventsForDashboard(req.body.user_id, (err, hosting) => {
+      if(err){
+        console.error(err);
+        res.sendStatus(500);
+        return;
+      }
+      let newHosting = [];
+      let newEvent = {};
+      hosting.forEach((event)=>{
+        if (newEvent.title !==event.title){
+          if (newEvent.title){
+            newHosting.push(newEvent);
+            newEvent = {};
+          }
+          newEvent.title = event.title;
+          newEvent.pending = event.pending;
+        }else{
+          newEvent.pending += event.pending;
+        }
+      })
+      newHosting.push(newEvent);
+      
+      db.getNameAndLocation(req.body.user_id, (err, nameAndLocation) => {
+        if(err){
+          console.error(err);
+          res.sendStatus(500);
+          return;
+        }
+        res.send({attending, newHosting, nameAndLocation})
+      })
+    })
+  })
+})
 
+app.get('/eventInfo', (req, res) =>{
+  db.getEventInfoForConditionalRender(req.body.event_id, req.body.user_id, (err, info)=>{
+    if(err){
+      console.error(err);
+      res.sendStatus(500);
+      return;
+    }
+    if (info[0].host_id === req.body.user_id){
+      db.getEventInfoForHost(req.body.event_id,(err, eventInfo)=>{
+        if(err){
+          console.error(err);
+          res.sendStatus(500);
+          return;
+        }
+        res.send({access: 'host', eventInfo});
+      })
+    } else if (info[0].private === 0 || info[0].pending === 0){
+      db.getEventInfoForNonHost (req.body.event_id, true, (err, eventInfo) =>{
+        if(err){
+          console.error(err);
+          res.sendStatus(500);
+          return;
+        }
+        res.send({access: 'full', eventInfo});
+      })
+    } else {
+      db.getEventInfoForNonHost (req.body.event_id, false, (err, eventInfo) =>{
+        if(err){
+          console.error(err);
+          res.sendStatus(500);
+          return;
+        }
+        res.send({access: 'limited', eventInfo});
+      })
+    }
+  })
+})
 
 app.listen(Port, () => {
   console.log(`listening on port ${Port}`);
