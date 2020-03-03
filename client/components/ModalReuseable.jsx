18e7cdaae23d5c31896720dd66dd8e9ad5fbdd22
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-03-02 22:34:39 -0600

    Add custom button

--- client/components/ModalReuseable.jsx
+++ client/components/ModalReuseable.jsx
@@ -5,22 +5,20 @@ const ModalReuseable = ({
   body, title, handleShow, handleClose, show, buttonName,
 }) => (
   <div>
-    <Button variant="primary" onClick={handleShow}>
+    <button className="modal-button" type="submit" onClick={handleShow}>
       {buttonName}
-    </Button>
+    </button>
     <Modal show={show} onHide={handleClose} dialogClassName="modalStyles">
       <Modal.Header closeButton onClick={handleClose}>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
       <Modal.Body dialogClassName="modal-body">{body}</Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
+        <Button variant="secondary" onClick={handleClose} style={{ backgroundColor: 'orange' }}>
           Close Modal
         </Button>
       </Modal.Footer>
     </Modal>
-
-    <span>This is a modal</span>
   </div>
 );
 
