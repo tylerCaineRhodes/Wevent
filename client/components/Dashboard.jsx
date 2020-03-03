34d494e3f3d9d3beecfc780c1b6451670388faff
RoyOlivarez <royolivarez66@gmail.com>
2020-03-02 18:20:43 -0600

    added a Dashboard file to components

---
+++ client/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Title from './Title.jsx';
+
+const Dashboard = () => (
+  <div>
+    <div>
+      <Title />
+    </div>
+    <div className="hostingDash">
+      <h3>Hosting</h3>
+    </div>
+  </div>
+);
+
+export default Dashboard;
