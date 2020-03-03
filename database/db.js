095877c38ce37ca6a61a078aee97c93d1fde7f2d
Benjamin Pernick <bpernick@comcast.net>
2020-03-02 22:27:16 -0600

    configure dotenv file

--- database/db.js
+++ database/db.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql');
+require('dotenv').config()
 
 const db = mysql.createConnection({
-   host: 'localhost',
-   user: 'root',
-   password: 'Phs@203386',
-   database: 'Wevent',
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASS,
+   database: process.env.DB_NAME,
 });
-
-
+console.log(process.env.DB_USER)
+console.log(process.env.DB_HOST)
 db.connect(function(err) {
   if (err) {
     return console.error('error: ' + err.message);
