67068574dc18c4ebe165e7bacc7c4e0bd9e54181
Sager Williams <sagerwilliams0011@gmail.com>
2020-03-04 19:22:52 -0600

    Started work on landingpage

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -7,27 +7,29 @@ const LandingPage = ({
   loginDisplayName, loginPassword, handleLoginDisplaynameChange, handleLoginPasswordChange, handleLoginSubmit, openSignUpModal, closeSignUpModal, handleGuestSubmit,
 }) => (
   <div className="landingPage">
-    <Title buttonText="signup" buttonClass="createNewEvent-button" onClick={openSignUpModal} />
-    <Form className="login">
-      <Form.Group controlId="Username">
-        {/* <Form.Label>Welcome to Wevent</Form.Label> */}
-        <Form.Control type="username" placeholder="Enter Username" value={loginDisplayName} onChange={(e) => handleLoginDisplaynameChange(e.target.value)} />
-      </Form.Group>
+    <Title buttonText="Continue as Guest" buttonClass="guestButton" onClick={(e) => handleGuestSubmit(e)} />
+    <div className="landingPageContent">
+      <Form className="login">
+        <Form.Group controlId="Username" className="landingPageUsernameField">
+          {/* <Form.Label>Welcome to Wevent</Form.Label> */}
+          <Form.Control type="username" placeholder="Enter Username" value={loginDisplayName} onChange={(e) => handleLoginDisplaynameChange(e.target.value)} />
+        </Form.Group>
 
-      <Form.Group controlId="Password">
-        <Form.Control type="password" placeholder="Enter Password" value={loginPassword} onChange={(e) => handleLoginPasswordChange(e.target.value, 'loginPassword')} />
-      </Form.Group>
-      <Button variant="primary" type="submit" className="loginButton" onClick={handleLoginSubmit}>
-        Login
-      </Button>
-    </Form>
-    <div>
-      <span>
-        <div id="splashnoaccount">Dont have an account?</div>
-        <Button variant="primary" type="submit" id="guestButton" onClick={(e) => handleGuestSubmit(e)}>
-          Guest
+        <Form.Group controlId="Password" className="landingPagePasswordField">
+          <Form.Control type="password" placeholder="Enter Password" value={loginPassword} onChange={(e) => handleLoginPasswordChange(e.target.value, 'loginPassword')} />
+        </Form.Group>
+        <Button variant="primary" type="submit" className="loginButton" onClick={handleLoginSubmit}>
+          Login
         </Button>
-      </span>
+      </Form>
+      <div>
+        <span>
+          <div id="splashnoaccount">
+            <span id="noAccountText">Dont have an account?</span>
+            <span id="noAccountButton" onClick={openSignUpModal}>Sign Up</span>
+          </div>
+        </span>
+      </div>
     </div>
   </div>
 );
