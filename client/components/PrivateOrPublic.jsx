98487b4f0524759f7a2b42b338f58e8c860ecb50
RoyOlivarez <royolivarez66@gmail.com>
2020-03-05 10:27:36 -0600

    changed color scheme for agenda and toggle buttons

--- client/components/PrivateOrPublic.jsx
+++ client/components/PrivateOrPublic.jsx
@@ -9,10 +9,10 @@ const PrivateOrPublic = ({
 }) => (
   <>
     <FormControl>
-      <FormControlLabel labelPlacement="start" control={<Switch size="medium" label="Public" checked={filterPublicValue} onChange={(e, v) => handleFilterPublicChange(v)} />} label="Public" />
+      <FormControlLabel labelPlacement="start" control={<Switch size="medium" label="Public" checked={filterPublicValue} onChange={(e, v) => handleFilterPublicChange(v)} style={{ color: 'orange' }} />} label="Public" />
     </FormControl>
     <FormControl>
-      <FormControlLabel labelPlacement="start" control={<Switch size="medium" label="Private" checked={filterPrivateValue} onChange={(e, v) => handleFilterPrivateChange(v)} />} label="Private" />
+      <FormControlLabel labelPlacement="start" control={<Switch size="medium" label="Private" checked={filterPrivateValue} onChange={(e, v) => handleFilterPrivateChange(v)} style={{ color: '#fc6565' }}/>} label="Private" />
     </FormControl>
   </>
 );
