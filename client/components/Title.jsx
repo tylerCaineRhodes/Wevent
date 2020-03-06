8dc2d31dce4a9e8d4b7e49a174f0780550fd555b
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-03-05 23:07:00 -0600

    Mess up buttons for fun

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -7,7 +7,7 @@ const Title = ({
     <nav className="navbar navbar-light" style={{ backgroundColor: 'orange', color: 'white' }}>
       <span className="nav-brand mb-0 h1">Wevent</span>
       <>{page === 'MainPage' && loginDisplayName !== 'Guest' && <button className="dashboardButton" type="button" onClick={changePage}>User Dashboard</button>}</>
-      <button className={buttonClass} type="button" onClick={onClick}>{buttonText}</button>
+      <button className="importantButton" type="button" onClick={onClick}>{buttonText}</button>
       {page === 'MainPage' && loginDisplayName !== 'Guest' && <button className="logoutButton" type="button" onClick={handleGuestBackToLandingPage}>Log Out</button>}
     </nav>
   </div>
