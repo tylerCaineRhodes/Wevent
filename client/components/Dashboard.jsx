ea28a96595de7bb782d88b2d45bda14ef9f126f5
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-03-05 18:34:44 -0600

    Basic style for user bashboard

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -16,26 +16,25 @@ class Dashboard extends React.Component {
       <div>
         <Title buttonText="Back" buttonClass="createNewEvent-button" onClick={this.props.changePage} />
         <div className="outerDash">
-          <p>{this.props.info.nameAndLocation[0].display_name}</p>
-          <p>{this.props.info.nameAndLocation[0].location_state}</p>
-          <p>{this.props.info.nameAndLocation[0].location_city}</p>
+          <span id="displayName">{this.props.info.nameAndLocation[0].display_name}</span>
+          <span id="location">{this.props.info.nameAndLocation[0].location_city}</span>
+          <span>{this.props.info.nameAndLocation[0].location_state}</span>
           <div className="hostingDash">
-            <h4>Hosting</h4>
+            <h4 className="h4">Hosting</h4>
             <>{this.props.info.hosting.map((event, i) => {
               let pending;
               if (event.pending >= 1) {
                 pending = 1;
               }
-              return (<div data-eventid={event.event_id} key={i} className={`hosting-dash-pending${pending}`} onClick={this.onClick.bind(this)}>{event.title}</div>);
+              return (<div data-eventid={event.event_id} key={i} className={`hosting-dash-pending${pending} dashboardListItem`} onClick={this.onClick.bind(this)}>{event.title}</div>);
             })}
             </>
           </div>
           <div className="attendingDash">
-            <h4>Attending</h4>
-            <>{this.props.info.attending.map((event, i) => (<div data-eventid={event.event_id} key={i} className={`attending-dash-pending${event.pending}`} onClick={this.onClick.bind(this)}>{event.title}</div>))}</>
+            <h4 className="h4">Attending</h4>
+            <>{this.props.info.attending.map((event, i) => (<div data-eventid={event.event_id} key={i} className={`attending-dash-pending${event.pending} dashboardListItem`} onClick={this.onClick.bind(this)}>{event.title}</div>))}</>
           </div>
         </div>
-        <button type="button" onClick={this.props.changePage}>Main Page</button>
       </div>
     );
   }
