608c6a9be8f57b6198e1e6c9c677fda5210e5ddd
Jordan Worm <jordan.worm96@gmail.com>
2020-03-06 12:00:42 -0600

    started styling the calendar

--- client/components/MainPage.jsx
+++ client/components/MainPage.jsx
@@ -25,6 +25,7 @@ const MainPage = ({
   states,
 }) => (
   <div id="mainPage">
+    <img className="mainPageBackgroundImage" src="./img/mainPage.jpg" />
     <Title
       handleGuestBackToLandingPage={handleGuestBackToLandingPage}
       loginDisplayName={loginDisplayName}
