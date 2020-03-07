05943ad12944c88172d145cacf9e10930e50fac8
Mark Wallace <mwallace999@gmail.com>
2020-03-07 17:03:02 -0600

    Update with error

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -5,7 +5,7 @@ const Title = ({
 }) => (
   <div>
     <nav className="navbar navbar-light">
-      <span className="nav-brand mb-0 h1"><img src='./img/NavbarLogoShadow2.png' className="navbarLogo" /></span>
+      <span className="nav-brand mb-0 h1"><img src='./img/NavbarLogoShadow.png' className="navbarLogo" /></span>
       <>{page === 'MainPage' && loginDisplayName !== 'Guest' && <button className="dashboardButton" type="button" onClick={changePage}>User Dashboard</button>}</>
       <button className={buttonClass} type="button" onClick={onClick}>{buttonText}</button>
       {/* {page === 'MainPage' && loginDisplayName !== 'Guest' && <button className="logoutButton" type="button" onClick={handleGuestBackToLandingPage}>Log Out</button>} */}
